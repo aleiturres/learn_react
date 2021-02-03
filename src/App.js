5cf9ad7a23006ba9d7b30de89c3f5ab2c62db2c8
Alexandre Iturres <aleiturres@hotmail.com>
2021-02-03 20:15:00 -0300

    Class 2

--- src/App.js
+++ src/App.js
@@ -1,20 +1,66 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Person from './Person/Person.js';
 
 class App extends Component {
+
+  state = {
+    persons: [
+      {name: 'Max', age: 29},
+      {name: 'Manu', age: 39},
+      {name: 'Sthepanie', age: 49},
+    ],
+    otherState: 'some other value'
+  }
+
+  switchNameHandler = (newName) => {
+    // console.log('Was Click!');
+    // this.state.persons[0].name = 'Alexandre';
+    this.setState({persons:  [
+      {name: newName, age: 29},
+      {name: 'Manu', age: 39},
+      {name: 'Sthepanie', age: 49},
+    ]})
+  }
+
+  nameChangedHandler = (event) => {
+    console.log(event.target.value);
+    this.setState({persons:  [
+      {name: 'Max', age: 29},
+      {name: event.target.value, age: 39},
+      {name: 'Sthepanie', age: 49},
+    ]})
+  }
+
   render() {
+    const style = {
+      backgroundColor: 'white',
+      font: 'inherit',
+      border: '1px solid blue',
+      padding:  '8px',
+      cursor: 'pointer'
+    }
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>Ola</h1>
+        <p>sadjhsadhs</p>
+        <button style={style} onClick={() => this.switchNameHandler('Best')}>Switch Name</button>
+        <Person 
+          name={this.state.persons[0].name} 
+          age={this.state.persons[0].age}
+          >
+        </Person>
+        <Person 
+          click={this.switchNameHandler.bind(this, 'ollslldkdk')}
+          name={this.state.persons[1].name} 
+          age={this.state.persons[1].age}
+          changed={this.nameChangedHandler}
+          >
+        </Person>
       </div>
     );
+    // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Ola'));
   }
 }
 
